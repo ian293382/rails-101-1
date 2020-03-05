f9b267306407afa1581c654a7917820d99065ea5
ian293382 <moshou5571228@gmail.com>
2020-03-05 16:32:25 +0800

    implement my posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,8 @@ class PostsController < ApplicationController
     end
   end
 
+  
+
  private
 
  def post_params
@@ -28,4 +30,11 @@ class PostsController < ApplicationController
  end
 
 
+end
+
+class Account::PostsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @posts = current_user.posts
+  end
 end
